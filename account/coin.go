a1e9d112d30b617cda44c1a3131a0dffa7731412
Clairvoyant1236 <guanyx2007@Hotmail.com>
2017-11-20 17:25:29 +0800

    Add version to cli wallet
    make wallet to be forward compatible by using version field

--- account/coin.go
+++ account/coin.go
@@ -19,21 +19,30 @@ type Coin struct {
 	AddressType AddressType
 }
 
-func (coin *Coin) Serialize(w io.Writer) error {
+func (coin *Coin) Serialize(w io.Writer, version string) error {
 	coin.Output.Serialize(w)
-	w.Write([]byte{byte(coin.AddressType)})
-
+	switch {
+	case version > "1.0.0":
+		w.Write([]byte{byte(coin.AddressType)})
+	default:
+		break
+	}
 	return nil
 }
 
-func (coin *Coin) Deserialize(r io.Reader) error {
+func (coin *Coin) Deserialize(r io.Reader, version string) error {
 	coin.Output = new(transaction.TxOutput)
 	coin.Output.Deserialize(r)
-	addrType, err := serialization.ReadUint8(r)
-	if err != nil {
-		return err
+	switch {
+	case version > "1.0.0":
+		addrType, err := serialization.ReadUint8(r)
+		if err != nil {
+			return err
+		}
+		coin.AddressType = AddressType(addrType)
+	default:
+		break
 	}
-	coin.AddressType = AddressType(addrType)
 
 	return nil
 }
