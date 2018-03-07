AndreasAhrens/ft-blockchain

Smart Contract version 0.5 released
Merge smart contract which committed by tanZiWen
Invoke bug fixed
Code format
Add cli cmd to deploy/invoke contract

core/code/FunctionCode.go
@@ -1,10 +1,10 @@
 package code
 
 import (
-	"DNA/common/log"
 	. "DNA/common"
-	. "DNA/core/contract"
+	"DNA/common/log"
 	"DNA/common/serialization"
+	. "DNA/core/contract"
 	"fmt"
 	"io"
 )
@@ -17,33 +17,43 @@ type FunctionCode struct {
 	ParameterTypes []ContractParameterType
 
 	// Contract return type list
-	ReturnTypes []ContractParameterType
+	ReturnType ContractParameterType
+
+	codeHash Uint160
 }
 
 // method of SerializableData
 func (fc *FunctionCode) Serialize(w io.Writer) error {
-	err := serialization.WriteVarBytes(w,ContractParameterTypeToByte(fc.ParameterTypes))
+	err := serialization.WriteByte(w, byte(fc.ReturnType))
 	if err != nil {
 		return err
 	}
-
-	err = serialization.WriteVarBytes(w,fc.Code)
+	err = serialization.WriteVarBytes(w, ContractParameterTypeToByte(fc.ParameterTypes))
 	if err != nil {
 		return err
 	}
-
+	err = serialization.WriteVarBytes(w, fc.Code)
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
 // method of SerializableData
 func (fc *FunctionCode) Deserialize(r io.Reader) error {
-	p,err := serialization.ReadVarBytes(r)
+	returnType, err := serialization.ReadByte(r)
 	if err != nil {
 		return err
 	}
-	fc.ParameterTypes = ByteToContractParameterType(p)
+	fc.ReturnType = ContractParameterType(returnType)
 
-	fc.Code,err = serialization.ReadVarBytes(r)
+	parameterTypes, err := serialization.ReadVarBytes(r)
+	if err != nil {
+		return err
+	}
+	fc.ParameterTypes = ByteToContractParameterType(parameterTypes)
+
+	fc.Code, err = serialization.ReadVarBytes(r)
 	if err != nil {
 		return err
 	}
@@ -65,18 +75,21 @@ func (fc *FunctionCode) GetParameterTypes() []ContractParameterType {
 
 // method of ICode
 // Get the list of return value
-func (fc *FunctionCode) GetReturnTypes() []ContractParameterType {
-	return fc.ReturnTypes
+func (fc *FunctionCode) GetReturnType() ContractParameterType {
+	return fc.ReturnType
 }
 
 // method of ICode
 // Get the hash of the smart contract
 func (fc *FunctionCode) CodeHash() Uint160 {
-	hash,err := ToCodeHash(fc.Code)
-	if err != nil {
-		log.Debug( fmt.Sprintf("[FunctionCode] ToCodeHash err=%s",err) )
-		return Uint160{0}
+	u160 := Uint160{}
+	if fc.codeHash == u160 {
+		u160, err := ToCodeHash(fc.Code)
+		if err != nil {
+			log.Debug(fmt.Sprintf("[FunctionCode] ToCodeHash err=%s", err))
+			return u160
+		}
+		fc.codeHash = u160
 	}
-
-	return hash
+	return fc.codeHash
 }

core/contract/program/ProgramBuilder.go
@@ -2,9 +2,9 @@ package program
 
 import (
 	. "DNA/common"
-	"DNA/vm"
 	"bytes"
 	"math/big"
+	"DNA/vm/avm"
 )
 
 type ProgramBuilder struct {
@@ -13,11 +13,11 @@ type ProgramBuilder struct {
 
 func NewProgramBuilder() *ProgramBuilder {
 	return &ProgramBuilder{
-	//TODO: add sync pool for create ProgramBuilder
+		//TODO: add sync pool for create ProgramBuilder
 	}
 }
 
-func (pb *ProgramBuilder) AddOp(op vm.OpCode) {
+func (pb *ProgramBuilder) AddOp(op avm.OpCode) {
 	pb.buffer.WriteByte(byte(op))
 }
 
@@ -27,15 +27,15 @@ func (pb *ProgramBuilder) AddCodes(codes []byte) {
 
 func (pb *ProgramBuilder) PushNumber(number *big.Int) {
 	if number.Cmp(big.NewInt(-1)) == 0 {
-		pb.AddOp(vm.PUSHM1)
+		pb.AddOp(avm.PUSHM1)
 		return
 	}
 	if number.Cmp(big.NewInt(0)) == 0 {
-		pb.AddOp(vm.PUSH0)
+		pb.AddOp(avm.PUSH0)
 		return
 	}
 	if number.Cmp(big.NewInt(0)) == 1 && number.Cmp(big.NewInt(16)) <= 0 {
-		pb.AddOp(vm.OpCode(byte(vm.PUSH1) - 1 + number.Bytes()[0]))
+		pb.AddOp(avm.OpCode(byte(avm.PUSH1) - 1 + number.Bytes()[0]))
 		return
 	}
 	pb.PushData(number.Bytes())
@@ -46,20 +46,20 @@ func (pb *ProgramBuilder) PushData(data []byte) {
 		return //TODO: add error
 	}
 
-	if len(data) <= int(vm.PUSHBYTES75) {
+	if len(data) <= int(avm.PUSHBYTES75) {
 		pb.buffer.WriteByte(byte(len(data)))
 		pb.buffer.Write(data[0:len(data)])
 	} else if len(data) < 0x100 {
-		pb.AddOp(vm.PUSHDATA1)
+		pb.AddOp(avm.PUSHDATA1)
 		pb.buffer.WriteByte(byte(len(data)))
 		pb.buffer.Write(data[0:len(data)])
 	} else if len(data) < 0x10000 {
-		pb.AddOp(vm.PUSHDATA2)
+		pb.AddOp(avm.PUSHDATA2)
 		dataByte := IntToBytes(len(data))
 		pb.buffer.Write(dataByte[0:2])
 		pb.buffer.Write(data[0:len(data)])
 	} else {
-		pb.AddOp(vm.PUSHDATA4)
+		pb.AddOp(avm.PUSHDATA4)
 		dataByte := IntToBytes(len(data))
 		pb.buffer.Write(dataByte[0:4])
 		pb.buffer.Write(data[0:len(data)])

core/signature/signature.go
@@ -6,7 +6,7 @@ import (
 	"DNA/core/contract/program"
 	"DNA/crypto"
 	. "DNA/errors"
-	"DNA/vm/interfaces"
+	"DNA/vm/avm/interfaces"
 	"bytes"
 	"crypto/sha256"
 	"io"
@@ -14,7 +14,7 @@ import (
 
 //SignableData describe the data need be signed.
 type SignableData interface {
-	interfaces.ISignableObject
+	interfaces.ICodeContainer
 
 	//Get the the SignableData's program hashes
 	GetProgramHashes() ([]common.Uint160, error)

smartcontract/states/contractstate.go
@@ -1,0 +1,121 @@
+package states
+
+import (
+	"io"
+	. "DNA/errors"
+	"DNA/core/code"
+	"bytes"
+	"DNA/common/serialization"
+	"DNA/smartcontract/types"
+	"DNA/common"
+)
+
+type ContractState struct {
+	StateBase
+	Code *code.FunctionCode
+	Name string
+	Version string
+	Author string
+	Email string
+	Description string
+	Language types.LangType
+	ProgramHash common.Uint160
+}
+
+func(contractState *ContractState) Serialize(w io.Writer) error {
+	contractState.StateBase.Serialize(w)
+	err := contractState.Code.Serialize(w)
+	if err != nil {
+		return err
+	}
+	err = serialization.WriteVarString(w, contractState.Name)
+	if err != nil {
+		return err
+	}
+	err = serialization.WriteVarString(w, contractState.Version)
+	if err != nil {
+		return err
+	}
+	err = serialization.WriteVarString(w, contractState.Author)
+	if err != nil {
+		return err
+	}
+	err = serialization.WriteVarString(w, contractState.Email)
+	if err != nil {
+		return err
+	}
+	err = serialization.WriteVarString(w, contractState.Description)
+	if err != nil {
+		return err
+	}
+	err = serialization.WriteByte(w, byte(contractState.Language))
+	if err != nil {
+		return err
+	}
+	_, err = contractState.ProgramHash.Serialize(w)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func(contractState *ContractState) Deserialize(r io.Reader) error {
+	stateBase := new(StateBase)
+	err := stateBase.Deserialize(r)
+	if err != nil {
+		return err
+	}
+	contractState.StateBase = *stateBase
+	f := new(code.FunctionCode)
+	err = f.Deserialize(r)
+	if err != nil {
+		return NewDetailErr(err, ErrNoCode, "ContractState Code Deserialize fail.")
+	}
+	contractState.Code = f
+	contractState.Name, err = serialization.ReadVarString(r)
+	if err != nil {
+		return NewDetailErr(err, ErrNoCode, "ContractState Name Deserialize fail.")
+
+	}
+	contractState.Version, err = serialization.ReadVarString(r)
+	if err != nil {
+		return NewDetailErr(err, ErrNoCode, "ContractState Version Deserialize fail.")
+
+	}
+	contractState.Author, err = serialization.ReadVarString(r)
+	if err != nil {
+		return NewDetailErr(err, ErrNoCode, "ContractState Author Deserialize fail.")
+
+	}
+	contractState.Email, err = serialization.ReadVarString(r)
+	if err != nil {
+		return NewDetailErr(err, ErrNoCode, "ContractState Email Deserialize fail.")
+
+	}
+	contractState.Description, err = serialization.ReadVarString(r)
+	if err != nil {
+		return NewDetailErr(err, ErrNoCode, "ContractState Description Deserialize fail.")
+
+	}
+	language, err := serialization.ReadByte(r)
+	if err != nil {
+		return NewDetailErr(err, ErrNoCode, "ContractState Description Deserialize fail.")
+
+	}
+	contractState.Language = types.LangType(language)
+	u := new(common.Uint160)
+	err = u.Deserialize(r)
+	if err != nil {
+		return err
+	}
+	contractState.ProgramHash = *u
+	return nil
+}
+
+func(contractState *ContractState) ToArray() []byte {
+	b := new(bytes.Buffer)
+	contractState.Serialize(b)
+	return b.Bytes()
+}
+
+

vm/avm/execution_context.go
@@ -1,0 +1,64 @@
+package avm
+
+import (
+	"DNA/vm/avm/utils"
+	"io"
+	"DNA/vm/avm/types"
+)
+
+type ExecutionContext struct {
+	Code               []byte
+	OpReader           *utils.VmReader
+	PushOnly           bool
+	BreakPoints        []uint
+	InstructionPointer int
+	CodeHash           []byte
+	engine             *ExecutionEngine
+}
+
+func NewExecutionContext(engine *ExecutionEngine, code []byte, pushOnly bool, breakPoints []uint) *ExecutionContext {
+	var executionContext ExecutionContext
+	executionContext.Code = code
+	executionContext.OpReader = utils.NewVmReader(code)
+	executionContext.PushOnly = pushOnly
+	executionContext.BreakPoints = breakPoints
+	executionContext.InstructionPointer = 0
+	executionContext.engine = engine
+	return &executionContext
+}
+
+func (ec *ExecutionContext) GetInstructionPointer() int {
+	return ec.OpReader.Position()
+}
+
+func (ec *ExecutionContext) SetInstructionPointer(offset int64) {
+	ec.OpReader.Seek(offset, io.SeekStart)
+}
+
+func (ec *ExecutionContext) GetCodeHash() []byte {
+	if ec.CodeHash == nil {
+		ec.CodeHash = ec.engine.crypto.Hash160(ec.Code)
+	}
+	return ec.CodeHash
+}
+
+func (ec *ExecutionContext) NextInstruction() OpCode {
+	return OpCode(ec.Code[ec.OpReader.Position()])
+}
+
+func (ec *ExecutionContext) Clone() *ExecutionContext {
+	executionContext := NewExecutionContext(ec.engine, ec.Code, ec.PushOnly, ec.BreakPoints)
+	executionContext.InstructionPointer = ec.InstructionPointer
+	executionContext.SetInstructionPointer(int64(ec.GetInstructionPointer()))
+	return executionContext
+}
+
+func (ec *ExecutionContext) GetStackItem() types.StackItemInterface {
+	return nil
+}
+
+func (ec *ExecutionContext) GetExecutionContext() *ExecutionContext {
+	return ec
+}
+
+

vm/avm/types/interop.go
@@ -1,0 +1,43 @@
+package types
+
+import (
+	"math/big"
+	"DNA/vm/avm/interfaces"
+)
+
+type InteropInterface struct {
+	_object interfaces.IInteropInterface
+}
+
+func NewInteropInterface(value interfaces.IInteropInterface) *InteropInterface {
+	var ii InteropInterface
+	ii._object = value
+	return &ii
+}
+
+func (ii *InteropInterface) Equals(other StackItemInterface) bool {
+	return false
+}
+
+func (ii *InteropInterface) GetBigInteger() *big.Int {
+	return big.NewInt(0)
+}
+
+func (ii *InteropInterface) GetBoolean() bool {
+	if ii._object == nil {
+		return false
+	}
+	return true
+}
+
+func (ii *InteropInterface) GetByteArray() []byte {
+	return ii._object.ToArray()
+}
+
+func (ii *InteropInterface) GetInterface() interfaces.IInteropInterface {
+	return ii._object
+}
+
+func (ii *InteropInterface) GetArray() []StackItemInterface {
+	return []StackItemInterface{}
+}

vm/evm/contract.go
@@ -1,0 +1,46 @@
+package evm
+
+import (
+	"DNA/common"
+	"math/big"
+)
+
+
+type Contract struct {
+	Caller common.Uint160
+	Code []byte
+	CodeHash common.Uint160
+	Input []byte
+	value *big.Int
+	jumpdest destinations
+}
+
+func NewContract(caller common.Uint160) *Contract {
+	var contract Contract
+	contract.value = new(big.Int)
+	contract.jumpdest = make(destinations, 0)
+	contract.Caller = caller
+	return &contract
+}
+
+func (c *Contract) GetOp(n uint64) OpCode {
+	return OpCode(c.GetByte(n))
+}
+
+func (c *Contract) GetByte(n uint64) byte {
+	if n < uint64(len(c.Code)) {
+		return c.Code[n]
+	}
+	return 0
+}
+
+func (c *Contract) SetCode(code []byte, codeHash common.Uint160) {
+	c.Code = code
+	c.CodeHash = codeHash
+}
+
+func (c *Contract) SetCallCode(code, input []byte, codeHash common.Uint160) {
+	c.Code = code
+	c.Input = input
+	c.CodeHash = codeHash
+}

vm/evm/crypto/crypto.go
@@ -1,0 +1,38 @@
+package crypto
+
+import (
+	"github.com/ethereum/go-ethereum/crypto/sha3"
+	"math/big"
+	"DNA/vm/evm/common"
+	"github.com/ethereum/go-ethereum/crypto/secp256k1"
+)
+
+var (
+	secp256k1_N, _  = new(big.Int).SetString("fffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141", 16)
+	secp256k1_halfN = new(big.Int).Div(secp256k1_N, big.NewInt(2))
+)
+
+func Keccak256(data ...[]byte) []byte {
+	d := sha3.NewKeccak256()
+	for _, b := range data {
+		d.Write(b)
+	}
+	return d.Sum(nil)
+}
+
+func ValidateSignatureValues(v byte, r, s *big.Int, homestead bool) bool {
+	if r.Cmp(common.Big1) < 0 || s.Cmp(common.Big1) < 0 {
+		return false
+	}
+	// reject upper range of s values (ECDSA malleability)
+	// see discussion in secp256k1/libsecp256k1/include/secp256k1.h
+	if homestead && s.Cmp(secp256k1_halfN) > 0 {
+		return false
+	}
+	// Frontier: allow s to be in full N range
+	return r.Cmp(secp256k1_N) < 0 && s.Cmp(secp256k1_N) < 0 && (v == 0 || v == 1)
+}
+
+func Ecrecover(hash, sig []byte) ([]byte, error) {
+	return secp256k1.RecoverPubkey(hash, sig)
+}

U-Network/UNetwork

core/code/FunctionCode.go
@@ -1,10 +1,10 @@
 package code
 
 import (
-	"UGCNetwork/common/log"
 	. "UGCNetwork/common"
-	. "UGCNetwork/core/contract"
+	"UGCNetwork/common/log"
 	"UGCNetwork/common/serialization"
+	. "UGCNetwork/core/contract"
 	"fmt"
 	"io"
 )
@@ -17,33 +17,43 @@ type FunctionCode struct {
 	ParameterTypes []ContractParameterType
 
 	// Contract return type list
-	ReturnTypes []ContractParameterType
+	ReturnType ContractParameterType
+
+	codeHash Uint160
 }
 
 // method of SerializableData
 func (fc *FunctionCode) Serialize(w io.Writer) error {
-	err := serialization.WriteVarBytes(w,ContractParameterTypeToByte(fc.ParameterTypes))
+	err := serialization.WriteByte(w, byte(fc.ReturnType))
 	if err != nil {
 		return err
 	}
-
-	err = serialization.WriteVarBytes(w,fc.Code)
+	err = serialization.WriteVarBytes(w, ContractParameterTypeToByte(fc.ParameterTypes))
 	if err != nil {
 		return err
 	}
-
+	err = serialization.WriteVarBytes(w, fc.Code)
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
 // method of SerializableData
 func (fc *FunctionCode) Deserialize(r io.Reader) error {
-	p,err := serialization.ReadVarBytes(r)
+	returnType, err := serialization.ReadByte(r)
 	if err != nil {
 		return err
 	}
-	fc.ParameterTypes = ByteToContractParameterType(p)
+	fc.ReturnType = ContractParameterType(returnType)
 
-	fc.Code,err = serialization.ReadVarBytes(r)
+	parameterTypes, err := serialization.ReadVarBytes(r)
+	if err != nil {
+		return err
+	}
+	fc.ParameterTypes = ByteToContractParameterType(parameterTypes)
+
+	fc.Code, err = serialization.ReadVarBytes(r)
 	if err != nil {
 		return err
 	}
@@ -65,18 +75,21 @@ func (fc *FunctionCode) GetParameterTypes() []ContractParameterType {
 
 // method of ICode
 // Get the list of return value
-func (fc *FunctionCode) GetReturnTypes() []ContractParameterType {
-	return fc.ReturnTypes
+func (fc *FunctionCode) GetReturnType() ContractParameterType {
+	return fc.ReturnType
 }
 
 // method of ICode
 // Get the hash of the smart contract
 func (fc *FunctionCode) CodeHash() Uint160 {
-	hash,err := ToCodeHash(fc.Code)
-	if err != nil {
-		log.Debug( fmt.Sprintf("[FunctionCode] ToCodeHash err=%s",err) )
-		return Uint160{0}
+	u160 := Uint160{}
+	if fc.codeHash == u160 {
+		u160, err := ToCodeHash(fc.Code)
+		if err != nil {
+			log.Debug(fmt.Sprintf("[FunctionCode] ToCodeHash err=%s", err))
+			return u160
+		}
+		fc.codeHash = u160
 	}
-
-	return hash
+	return fc.codeHash
 }

core/contract/program/ProgramBuilder.go
@@ -2,9 +2,9 @@ package program
 
 import (
 	. "UGCNetwork/common"
-	"UGCNetwork/vm"
 	"bytes"
 	"math/big"
+	"UGCNetwork/vm/avm"
 )
 
 type ProgramBuilder struct {
@@ -13,11 +13,11 @@ type ProgramBuilder struct {
 
 func NewProgramBuilder() *ProgramBuilder {
 	return &ProgramBuilder{
-	//TODO: add sync pool for create ProgramBuilder
+		//TODO: add sync pool for create ProgramBuilder
 	}
 }
 
-func (pb *ProgramBuilder) AddOp(op vm.OpCode) {
+func (pb *ProgramBuilder) AddOp(op avm.OpCode) {
 	pb.buffer.WriteByte(byte(op))
 }
 
@@ -27,15 +27,15 @@ func (pb *ProgramBuilder) AddCodes(codes []byte) {
 
 func (pb *ProgramBuilder) PushNumber(number *big.Int) {
 	if number.Cmp(big.NewInt(-1)) == 0 {
-		pb.AddOp(vm.PUSHM1)
+		pb.AddOp(avm.PUSHM1)
 		return
 	}
 	if number.Cmp(big.NewInt(0)) == 0 {
-		pb.AddOp(vm.PUSH0)
+		pb.AddOp(avm.PUSH0)
 		return
 	}
 	if number.Cmp(big.NewInt(0)) == 1 && number.Cmp(big.NewInt(16)) <= 0 {
-		pb.AddOp(vm.OpCode(byte(vm.PUSH1) - 1 + number.Bytes()[0]))
+		pb.AddOp(avm.OpCode(byte(avm.PUSH1) - 1 + number.Bytes()[0]))
 		return
 	}
 	pb.PushData(number.Bytes())
@@ -46,20 +46,20 @@ func (pb *ProgramBuilder) PushData(data []byte) {
 		return //TODO: add error
 	}
 
-	if len(data) <= int(vm.PUSHBYTES75) {
+	if len(data) <= int(avm.PUSHBYTES75) {
 		pb.buffer.WriteByte(byte(len(data)))
 		pb.buffer.Write(data[0:len(data)])
 	} else if len(data) < 0x100 {
-		pb.AddOp(vm.PUSHDATA1)
+		pb.AddOp(avm.PUSHDATA1)
 		pb.buffer.WriteByte(byte(len(data)))
 		pb.buffer.Write(data[0:len(data)])
 	} else if len(data) < 0x10000 {
-		pb.AddOp(vm.PUSHDATA2)
+		pb.AddOp(avm.PUSHDATA2)
 		dataByte := IntToBytes(len(data))
 		pb.buffer.Write(dataByte[0:2])
 		pb.buffer.Write(data[0:len(data)])
 	} else {
-		pb.AddOp(vm.PUSHDATA4)
+		pb.AddOp(avm.PUSHDATA4)
 		dataByte := IntToBytes(len(data))
 		pb.buffer.Write(dataByte[0:4])
 		pb.buffer.Write(data[0:len(data)])

core/signature/signature.go
@@ -6,7 +6,7 @@ import (
 	"UGCNetwork/core/contract/program"
 	"UGCNetwork/crypto"
 	. "UGCNetwork/errors"
-	"UGCNetwork/vm/interfaces"
+	"UGCNetwork/vm/avm/interfaces"
 	"bytes"
 	"crypto/sha256"
 	"io"
@@ -14,7 +14,7 @@ import (
 
 //SignableData describe the data need be signed.
 type SignableData interface {
-	interfaces.ISignableObject
+	interfaces.ICodeContainer
 
 	//Get the the SignableData's program hashes
 	GetProgramHashes() ([]common.Uint160, error)

smartcontract/states/contractstate.go
@@ -1,0 +1,121 @@
+package states
+
+import (
+	"io"
+	. "UGCNetwork/errors"
+	"UGCNetwork/core/code"
+	"bytes"
+	"UGCNetwork/common/serialization"
+	"UGCNetwork/smartcontract/types"
+	"UGCNetwork/common"
+)
+
+type ContractState struct {
+	StateBase
+	Code *code.FunctionCode
+	Name string
+	Version string
+	Author string
+	Email string
+	Description string
+	Language types.LangType
+	ProgramHash common.Uint160
+}
+
+func(contractState *ContractState) Serialize(w io.Writer) error {
+	contractState.StateBase.Serialize(w)
+	err := contractState.Code.Serialize(w)
+	if err != nil {
+		return err
+	}
+	err = serialization.WriteVarString(w, contractState.Name)
+	if err != nil {
+		return err
+	}
+	err = serialization.WriteVarString(w, contractState.Version)
+	if err != nil {
+		return err
+	}
+	err = serialization.WriteVarString(w, contractState.Author)
+	if err != nil {
+		return err
+	}
+	err = serialization.WriteVarString(w, contractState.Email)
+	if err != nil {
+		return err
+	}
+	err = serialization.WriteVarString(w, contractState.Description)
+	if err != nil {
+		return err
+	}
+	err = serialization.WriteByte(w, byte(contractState.Language))
+	if err != nil {
+		return err
+	}
+	_, err = contractState.ProgramHash.Serialize(w)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func(contractState *ContractState) Deserialize(r io.Reader) error {
+	stateBase := new(StateBase)
+	err := stateBase.Deserialize(r)
+	if err != nil {
+		return err
+	}
+	contractState.StateBase = *stateBase
+	f := new(code.FunctionCode)
+	err = f.Deserialize(r)
+	if err != nil {
+		return NewDetailErr(err, ErrNoCode, "ContractState Code Deserialize fail.")
+	}
+	contractState.Code = f
+	contractState.Name, err = serialization.ReadVarString(r)
+	if err != nil {
+		return NewDetailErr(err, ErrNoCode, "ContractState Name Deserialize fail.")
+
+	}
+	contractState.Version, err = serialization.ReadVarString(r)
+	if err != nil {
+		return NewDetailErr(err, ErrNoCode, "ContractState Version Deserialize fail.")
+
+	}
+	contractState.Author, err = serialization.ReadVarString(r)
+	if err != nil {
+		return NewDetailErr(err, ErrNoCode, "ContractState Author Deserialize fail.")
+
+	}
+	contractState.Email, err = serialization.ReadVarString(r)
+	if err != nil {
+		return NewDetailErr(err, ErrNoCode, "ContractState Email Deserialize fail.")
+
+	}
+	contractState.Description, err = serialization.ReadVarString(r)
+	if err != nil {
+		return NewDetailErr(err, ErrNoCode, "ContractState Description Deserialize fail.")
+
+	}
+	language, err := serialization.ReadByte(r)
+	if err != nil {
+		return NewDetailErr(err, ErrNoCode, "ContractState Description Deserialize fail.")
+
+	}
+	contractState.Language = types.LangType(language)
+	u := new(common.Uint160)
+	err = u.Deserialize(r)
+	if err != nil {
+		return err
+	}
+	contractState.ProgramHash = *u
+	return nil
+}
+
+func(contractState *ContractState) ToArray() []byte {
+	b := new(bytes.Buffer)
+	contractState.Serialize(b)
+	return b.Bytes()
+}
+
+

vm/avm/execution_context.go
@@ -1,0 +1,64 @@
+package avm
+
+import (
+	"UGCNetwork/vm/avm/utils"
+	"io"
+	"UGCNetwork/vm/avm/types"
+)
+
+type ExecutionContext struct {
+	Code               []byte
+	OpReader           *utils.VmReader
+	PushOnly           bool
+	BreakPoints        []uint
+	InstructionPointer int
+	CodeHash           []byte
+	engine             *ExecutionEngine
+}
+
+func NewExecutionContext(engine *ExecutionEngine, code []byte, pushOnly bool, breakPoints []uint) *ExecutionContext {
+	var executionContext ExecutionContext
+	executionContext.Code = code
+	executionContext.OpReader = utils.NewVmReader(code)
+	executionContext.PushOnly = pushOnly
+	executionContext.BreakPoints = breakPoints
+	executionContext.InstructionPointer = 0
+	executionContext.engine = engine
+	return &executionContext
+}
+
+func (ec *ExecutionContext) GetInstructionPointer() int {
+	return ec.OpReader.Position()
+}
+
+func (ec *ExecutionContext) SetInstructionPointer(offset int64) {
+	ec.OpReader.Seek(offset, io.SeekStart)
+}
+
+func (ec *ExecutionContext) GetCodeHash() []byte {
+	if ec.CodeHash == nil {
+		ec.CodeHash = ec.engine.crypto.Hash160(ec.Code)
+	}
+	return ec.CodeHash
+}
+
+func (ec *ExecutionContext) NextInstruction() OpCode {
+	return OpCode(ec.Code[ec.OpReader.Position()])
+}
+
+func (ec *ExecutionContext) Clone() *ExecutionContext {
+	executionContext := NewExecutionContext(ec.engine, ec.Code, ec.PushOnly, ec.BreakPoints)
+	executionContext.InstructionPointer = ec.InstructionPointer
+	executionContext.SetInstructionPointer(int64(ec.GetInstructionPointer()))
+	return executionContext
+}
+
+func (ec *ExecutionContext) GetStackItem() types.StackItemInterface {
+	return nil
+}
+
+func (ec *ExecutionContext) GetExecutionContext() *ExecutionContext {
+	return ec
+}
+
+

vm/avm/types/interop.go
@@ -1,0 +1,43 @@
+package types
+
+import (
+	"math/big"
+	"UGCNetwork/vm/avm/interfaces"
+)
+
+type InteropInterface struct {
+	_object interfaces.IInteropInterface
+}
+
+func NewInteropInterface(value interfaces.IInteropInterface) *InteropInterface {
+	var ii InteropInterface
+	ii._object = value
+	return &ii
+}
+
+func (ii *InteropInterface) Equals(other StackItemInterface) bool {
+	return false
+}
+
+func (ii *InteropInterface) GetBigInteger() *big.Int {
+	return big.NewInt(0)
+}
+
+func (ii *InteropInterface) GetBoolean() bool {
+	if ii._object == nil {
+		return false
+	}
+	return true
+}
+
+func (ii *InteropInterface) GetByteArray() []byte {
+	return ii._object.ToArray()
+}
+
+func (ii *InteropInterface) GetInterface() interfaces.IInteropInterface {
+	return ii._object
+}
+
+func (ii *InteropInterface) GetArray() []StackItemInterface {
+	return []StackItemInterface{}
+}

vm/evm/contract.go
@@ -1,0 +1,46 @@
+package evm
+
+import (
+	"UGCNetwork/common"
+	"math/big"
+)
+
+
+type Contract struct {
+	Caller common.Uint160
+	Code []byte
+	CodeHash common.Uint160
+	Input []byte
+	value *big.Int
+	jumpdest destinations
+}
+
+func NewContract(caller common.Uint160) *Contract {
+	var contract Contract
+	contract.value = new(big.Int)
+	contract.jumpdest = make(destinations, 0)
+	contract.Caller = caller
+	return &contract
+}
+
+func (c *Contract) GetOp(n uint64) OpCode {
+	return OpCode(c.GetByte(n))
+}
+
+func (c *Contract) GetByte(n uint64) byte {
+	if n < uint64(len(c.Code)) {
+		return c.Code[n]
+	}
+	return 0
+}
+
+func (c *Contract) SetCode(code []byte, codeHash common.Uint160) {
+	c.Code = code
+	c.CodeHash = codeHash
+}
+
+func (c *Contract) SetCallCode(code, input []byte, codeHash common.Uint160) {
+	c.Code = code
+	c.Input = input
+	c.CodeHash = codeHash
+}

vm/evm/crypto/crypto.go
@@ -1,0 +1,38 @@
+package crypto
+
+import (
+	"github.com/ethereum/go-ethereum/crypto/sha3"
+	"math/big"
+	"UGCNetwork/vm/evm/common"
+	"github.com/ethereum/go-ethereum/crypto/secp256k1"
+)
+
+var (
+	secp256k1_N, _  = new(big.Int).SetString("fffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141", 16)
+	secp256k1_halfN = new(big.Int).Div(secp256k1_N, big.NewInt(2))
+)
+
+func Keccak256(data ...[]byte) []byte {
+	d := sha3.NewKeccak256()
+	for _, b := range data {
+		d.Write(b)
+	}
+	return d.Sum(nil)
+}
+
+func ValidateSignatureValues(v byte, r, s *big.Int, homestead bool) bool {
+	if r.Cmp(common.Big1) < 0 || s.Cmp(common.Big1) < 0 {
+		return false
+	}
+	// reject upper range of s values (ECDSA malleability)
+	// see discussion in secp256k1/libsecp256k1/include/secp256k1.h
+	if homestead && s.Cmp(secp256k1_halfN) > 0 {
+		return false
+	}
+	// Frontier: allow s to be in full N range
+	return r.Cmp(secp256k1_N) < 0 && s.Cmp(secp256k1_N) < 0 && (v == 0 || v == 1)
+}
+
+func Ecrecover(hash, sig []byte) ([]byte, error) {
+	return secp256k1.RecoverPubkey(hash, sig)
+}