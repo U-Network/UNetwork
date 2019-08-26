d063c6220a8524efd750dd8707894d2fa871a22b
杨捷 <yangjie@jojoin.com>
2019-08-26 16:07:20 +0800

    Update ipfs.go
    add comments

--- ipfs/ipfs.go
+++ ipfs/ipfs.go
@@ -6,6 +6,13 @@
  */
 
 
+
+/*
+
+Handle starts handling the given signals, and will call the handler callback function each time a signal is caught. The function is passed the number of times the handler has been triggered in total, as well as the handler itself, so that the handling logic can use the handler's wait group to ensure clean shutdown when Close() is called.
+
+*/
+
 type IntrHandler struct {
     // contains filtered or unexported fields
 }
